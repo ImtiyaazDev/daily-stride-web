41f69930cd8a6df85af76b027dbdef526ff03d88
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-17 09:37:31 +0200

    config: Add custom fonts to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {}
+		fontFamily: {
+			sans: ["Roboto", "Arial", "Helvetica", "sans-serif"]
+		},
+		extend: {
+			fontFamily: {
+				heading: ["Poppins", "Arial", "Helvetica", "sans-serif"]
+			}
+		}
 	},
 	plugins: []
 };
