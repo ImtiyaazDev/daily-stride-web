059226e7a8a17d7100a3ac86e05737277371470f
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-17 13:28:02 +0200

    feat: Create data for features section

--- src/data/index.js
+++ src/data/index.js
@@ -26,4 +26,25 @@ const navLinks = [
 	}
 ];
 
-export { navLinks };
+const features = [
+	{
+		id: 1,
+		heading: "Habit Tracking",
+		tagline: "Stay on top of your goals with our intuitive habit tracker.",
+		text: "Whether it's daily workouts, reading, or mindfulness, track your habits effortlessly and stay motivated with visual progress indicators. See your consistency grow with every step you take."
+	},
+	{
+		id: 2,
+		heading: "Stats Monitoring",
+		tagline: "Visualise your progress with comprehensive analytics.",
+		text: "Get detailed insights into your progress with easy-to-read charts and stats. Watch your growth unfold over time and make data-driven decisions to fine-tune your personal goals."
+	},
+	{
+		id: 3,
+		heading: "Daily Journal",
+		tagline: "Reflect on your day and jot down your thoughts easily.",
+		text: "Capture your thoughts, feelings, and achievements in our integrated journal. It's your personal space to reflect and gain clarity on your journey, one day at a time."
+	}
+];
+
+export { navLinks, features };
