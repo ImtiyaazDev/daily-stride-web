60ad8504be19d0950a5efdd335f14ce247729e98
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-17 12:01:20 +0200

    feat: Add hero section to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./layout/Header";
+import Hero from "./layout/Hero";
 
 export default function App() {
 	return (
 		<div className="h-screen w-screen overflow-x-hidden">
 			<Header />
+			<main className="grid">
+				<Hero />
+			</main>
 		</div>
 	);
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -39,7 +39,10 @@ export default function Header() {
 						<IconSun />
 						<IconMoon />
 					</div>
-					<a className="bg-primaryRed-200 text-primaryRed-800 hover:bg-primaryRed-100 hover:border-primaryRed-800 focus:ring-offset-primaryRed-800 cursor-pointer rounded-full border-2 border-transparent px-3 py-2 outline-none transition-all duration-300 ease-in-out focus:ring focus:ring-transparent focus:ring-offset-2 md:px-4 md:py-3" href="#signup">
+					<a
+						className="bg-primaryRed-200 text-primaryRed-800 hover:bg-primaryRed-100 hover:border-primaryRed-800 focus:ring-offset-primaryRed-800 cursor-pointer rounded-full border-2 border-transparent px-3 py-2 outline-none transition-all duration-300 ease-in-out focus:ring focus:ring-transparent focus:ring-offset-2 md:px-4 md:py-3"
+						href="#signup"
+					>
 						Sign Up
 					</a>
 				</div>
