2a31d330c9b9ceca2eedf8570d27e7742efcc6be
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-17 15:10:30 +0200

    feat: Create responsive benefits section

---
+++ src/layout/Benefits.jsx
@@ -0,0 +1,50 @@
+export default function Benefits() {
+	return (
+		<section
+			className="lg:px-15 bg-primaryBlueLight-50 flex flex-col items-center gap-20 px-6 py-28 md:flex-row md:px-10"
+			id="benefits"
+		>
+			<div className="flex w-full flex-col items-start gap-4 md:max-w-[700px] lg:gap-6">
+				<div className="flex flex-col gap-2">
+					<h3 className="text-primaryRed-400 font-medium lg:text-xl">
+						Benefits
+					</h3>
+					<h2 className="font-heading text-primaryBlueLight-950 text-3xl font-bold lg:text-5xl">
+						Empower Your Growth
+					</h2>
+				</div>
+				<p className="text-lg">
+					Daily Stride is designed to help you cultivate positive habits and
+					gain insights into your self-improvement journey. By actively engaging
+					with your progress, you can make informed decisions and stay motivated
+				</p>
+				<div className="flex flex-col items-center gap-3 lg:flex-row lg:gap-4">
+					<div className="flex w-full flex-col gap-2 lg:gap-3">
+						<h4 className="font-heading text-primaryRed-400 text-xl font-medium lg:text-2xl">
+							Track Progess
+						</h4>
+						<p>
+							Visualise your achievements and stay motivated with comprehensive
+							analytics and insights.
+						</p>
+					</div>
+					<div className="flex w-full flex-col gap-2 lg:gap-3">
+						<h4 className="font-heading text-primaryRed-400 text-xl font-medium lg:text-2xl">
+							Reflect Daily
+						</h4>
+						<p>
+							Use our daily journal to reflect on your experiences and foster
+							personal growth.
+						</p>
+					</div>
+				</div>
+			</div>
+			<figure>
+				<img
+					src=""
+					alt="MOCKUP"
+				/>
+			</figure>
+		</section>
+	);
+}
