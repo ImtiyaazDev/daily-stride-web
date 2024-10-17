106189cf85d19c73efb74e2fa811eff25ab7ccba
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-17 11:27:04 +0200

    chore: Create objects with website links

---
+++ src/data/index.js
@@ -0,0 +1,29 @@
+const navLinks = [
+	{
+		id: 1,
+		href: "home",
+		text: "Home"
+	},
+	{
+		id: 2,
+		href: "features",
+		text: "Features"
+	},
+	{
+		id: 3,
+		href: "benefits",
+		text: "Benefits"
+	},
+	{
+		id: 4,
+		href: "testimonials",
+		text: "Testimonials"
+	},
+	{
+		id: 5,
+		href: "how-it-works",
+		text: "How It Works"
+	}
+];
+
+export { navLinks };
