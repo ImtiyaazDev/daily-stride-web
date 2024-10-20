0c0e6ff86422c6e18b5dfc1a05de3f356a5d4636
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-20 10:32:11 +0200

    animate: Add animation to hero section

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -1,4 +1,31 @@
+import { motion } from "framer-motion";
+
 export default function Hero() {
+	const variants = {
+		hidden: { opacity: 0, x: "-1000px" },
+		headingShow: {
+			opacity: 1,
+			x: "0px",
+			transition: { duration: 1, delay: 0.7, ease: "easeInOut" }
+		},
+		textShow: {
+			opacity: 1,
+			x: "0px",
+			transition: { duration: 1.5, delay: 1.2, ease: "easeInOut" }
+		},
+		linkShow: {
+			opacity: 1,
+			x: "0px",
+			transition: { duration: 1.5, delay: 1.2, ease: "easeInOut" }
+		},
+		imageHidden: { opacity: 0, y: "200px" },
+		imageShow: {
+			opacity: 1,
+			y: "0px",
+			transition: { duration: 1.2, delay: 1.5, ease: "easeInOut" }
+		}
+	};
+
 	return (
 		<section
 			className="lg:px-15 flex flex-col items-center gap-6 px-6 py-28 md:px-10"
@@ -6,30 +33,48 @@ export default function Hero() {
 		>
 			<div className="flex flex-col items-center gap-8">
 				<div className="flex max-w-[600px] flex-col items-center gap-5 text-center lg:max-w-[800px]">
-					<h1 className="font-heading text-primaryBlueLight-800 text-5xl font-bold lg:text-6xl">
+					<motion.h1
+						variants={variants}
+						initial="hidden"
+						animate="headingShow"
+						className="font-heading text-primaryBlueLight-800 text-5xl font-bold lg:text-6xl"
+					>
 						Transform Your Journey with Daily Stride
-					</h1>
-					<p className="lg:max-w-[600px]">
+					</motion.h1>
+					<motion.p
+						variants={variants}
+						initial="hidden"
+						animate="textShow"
+						className="lg:max-w-[600px]"
+					>
 						At Daily Stride, we provide you with the tools to build lasting
 						habits, track your progress, and reflect on your journey—all in one
 						intuitive platform. Start today and see the difference with each
 						step you take.
-					</p>
+					</motion.p>
 				</div>
-				<a
+				<motion.a
+					variants={variants}
+					initial="hidden"
+					animate="linkShow"
 					className="bg-primaryRed-200 text-primaryRed-800 hover:bg-primaryRed-100 hover:border-primaryRed-800 focus:ring-offset-primaryRed-800 cursor-pointer rounded-full border-2 border-transparent px-3 py-2 outline-none transition-all duration-300 ease-in-out focus:ring focus:ring-transparent focus:ring-offset-2 md:px-4 md:py-3"
 					href="#signup"
 				>
 					Get Started
-				</a>
+				</motion.a>
 			</div>
-			<figure className="w-96 sm:w-[450px] md:w-[700px] lg:w-[1000px]">
+			<motion.figure
+				variants={variants}
+				initial="imageHidden"
+				animate="imageShow"
+				className="w-96 sm:w-[450px] md:w-[700px] lg:w-[1000px]"
+			>
 				<img
 					src="/assets/hero.webp"
 					alt="Mockup of Daily Stride dashboard showcasing the main dashboard for desktops and phone devices."
 					loading="lazy"
 				/>
-			</figure>
+			</motion.figure>
 		</section>
 	);
 }
