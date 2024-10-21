12fb4c0abff5bf07890127aa3e091efa7c3525f5
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-21 11:05:19 +0200

    feat: Wrap app in dark mode context

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,23 @@ import Header from "./layout/Header";
 import Hero from "./layout/Hero";
 import HowItWorks from "./layout/HowItWorks";
 import Testimonials from "./layout/Testimonials";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 export default function App() {
 	return (
-		<div className="h-screen w-screen overflow-x-hidden">
-			<Header />
-			<main className="grid">
-				<Hero />
-				<Features />
-				<Benefits />
-				<Testimonials />
-				<HowItWorks />
-				<CallToAction />
-			</main>
-			<Footer />
-		</div>
+		<DarkModeProvider>
+			<div className="h-screen w-screen overflow-x-hidden">
+				<Header />
+				<main className="grid">
+					<Hero />
+					<Features />
+					<Benefits />
+					<Testimonials />
+					<HowItWorks />
+					<CallToAction />
+				</main>
+				<Footer />
+			</div>
+		</DarkModeProvider>
 	);
 }
