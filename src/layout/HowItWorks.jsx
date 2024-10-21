805aefae8f9488d96cdb16725a4f880e0bebfee4
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-21 09:01:51 +0200

    animate: Add animations to how it works section

--- src/layout/HowItWorks.jsx
+++ src/layout/HowItWorks.jsx
@@ -4,26 +4,68 @@ import {
 	IconClipboardText
 } from "@tabler/icons-react";
 import { userActions } from "../data";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 export default function HowItWorks() {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true, margin: "0px 0px -250px 0px" });
+
+	const contentVariants = {
+		hidden: { opacity: 0, y: "-50px" },
+		show: {
+			opacity: 1,
+			y: "0px",
+			transition: { duration: 0.7, ease: "easeInOut" }
+		}
+	};
+
+	const stepsVariants = {
+		hidden: { opacity: 0, y: 100 },
+		show: (index) => ({
+			opacity: 1,
+			y: 0,
+			transition: {
+				delay: 0.35 * index,
+				duration: 0.24
+			}
+		})
+	};
+
 	return (
 		<section
 			className="lg:px-15 flex flex-col items-center gap-20 px-6 py-28 md:px-10"
 			id="how-it-works"
+			ref={ref}
 		>
-			<div className="flex flex-col items-center gap-2 max-w-[500px] lg:max-w-[800px]">
-				<h3 className="text-primaryRed-400 font-medium lg:text-xl">
+			<div className="flex max-w-[500px] flex-col items-center gap-2 lg:max-w-[800px]">
+				<motion.h3
+					className="text-primaryRed-400 font-medium lg:text-xl"
+					variants={contentVariants}
+					initial="hidden"
+					animate={isInView ? "show" : "hidden"}
+				>
 					How It Works
-				</h3>
-				<h2 className="font-heading text-primaryBlueLight-950 text-center text-3xl font-bold lg:text-5xl">
+				</motion.h3>
+				<motion.h2
+					className="font-heading text-primaryBlueLight-950 text-center text-3xl font-bold lg:text-5xl"
+					variants={contentVariants}
+					initial="hidden"
+					animate={isInView ? "show" : "hidden"}
+				>
 					Achieve Your Goals in Three Simple Steps with Daily Stride
-				</h2>
+				</motion.h2>
 			</div>
-			<article className="flex flex-col gap-10 lg:flex-row lg: justify-between w-full">
-				{userActions.map((action) => (
-					<div
+			<article className="lg: flex w-full flex-col justify-between gap-10 lg:flex-row">
+				{userActions.map((action, index) => (
+					<motion.div
 						key={action.id}
-						className="flex w-full flex-col items-center text-center gap-3"
+						className="flex w-full flex-col items-center gap-3 text-center"
+						initial="hidden"
+						whileInView="show"
+						variants={stepsVariants}
+						viewport={{ once: true }}
+						custom={index}
 					>
 						{action?.tagline.includes("Sign Up") && (
 							<IconClick
@@ -47,7 +89,7 @@ export default function HowItWorks() {
 							{action.tagline}
 						</h4>
 						<p>{action.action}</p>
-					</div>
+					</motion.div>
 				))}
 			</article>
 		</section>
