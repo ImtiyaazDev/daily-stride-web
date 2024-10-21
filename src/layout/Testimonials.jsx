a2e9dd4ed31f5d56896cd73b8f88c695e81bb9f9
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-21 08:55:03 +0200

    animate: Add animations to testimonial section

--- src/layout/Testimonials.jsx
+++ src/layout/Testimonials.jsx
@@ -1,25 +1,67 @@
 import { IconStarFilled } from "@tabler/icons-react";
 import { testimonials } from "../data";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 export default function Testimonials() {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true, margin: "0px 0px -250px 0px" });
+
+	const contentVariants = {
+		hidden: { opacity: 0, x: "-200px" },
+		show: {
+			opacity: 1,
+			x: "0px",
+			transition: { duration: 0.7, ease: "easeInOut" }
+		}
+	};
+
+	const cardVariants = {
+		hidden: { opacity: 0, y: 100 },
+		show: (index) => ({
+			opacity: 1,
+			y: 0,
+			transition: {
+				delay: 0.35 * index,
+				duration: 0.24
+			}
+		})
+	};
+
 	return (
 		<section
 			className="lg:px-15 flex flex-col items-start gap-20 px-6 py-28 md:px-10"
 			id="testimonials"
+			ref={ref}
 		>
 			<div className="flex flex-col gap-2">
-				<h3 className="text-primaryRed-400 font-medium lg:text-xl">
+				<motion.h3
+					className="text-primaryRed-400 font-medium lg:text-xl"
+					variants={contentVariants}
+					initial="hidden"
+					animate={isInView ? "show" : "hidden"}
+				>
 					Testimonials
-				</h3>
-				<h2 className="font-heading text-primaryBlueLight-950 text-3xl font-bold lg:text-5xl">
+				</motion.h3>
+				<motion.h2
+					className="font-heading text-primaryBlueLight-950 text-3xl font-bold lg:text-5xl"
+					variants={contentVariants}
+					initial="hidden"
+					animate={isInView ? "show" : "hidden"}
+				>
 					What Our Users Say
-				</h2>
+				</motion.h2>
 			</div>
 			<article className="flex flex-col gap-7 lg:flex-row">
-				{testimonials.map((testimonial) => (
-					<div
+				{testimonials.map((testimonial, index) => (
+					<motion.div
 						key={testimonial.id}
 						className={`flex w-full flex-col gap-4 rounded-xl p-4 drop-shadow-sm lg:p-6 ${testimonial.redBackground ? "bg-primaryRed-50" : "bg-primaryBlueLight-50"}`}
+						initial="hidden"
+						whileInView="show"
+						custom={index}
+						viewport={{ once: true }}
+						variants={cardVariants}
 					>
 						<div className="flex items-center gap-1">
 							{Array(testimonial.rating)
@@ -50,7 +92,7 @@ export default function Testimonials() {
 								<p>{testimonial.position}</p>
 							</div>
 						</div>
-					</div>
+					</motion.div>
 				))}
 			</article>
 		</section>
