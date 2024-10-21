ce8dfa7d3cf69685fa04843ee593a1a9c44a221f
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-21 11:05:32 +0200

    feat: Toggle theme

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,11 +2,14 @@ import { IconMenu, IconMoon, IconSun, IconX } from "@tabler/icons-react";
 import { navLinks } from "../data/index";
 import { useEffect, useRef, useState } from "react";
 import { motion, useInView } from "framer-motion";
+import { useDarkMode } from "../context/DarkModeContext";
 
 export default function Header() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const ref = useRef(null);
 	const isInView = useInView(ref);
+	const { toggleDarkMode, isDarkMode } = useDarkMode();
+	console.log(isDarkMode);
 
 	const handleClick = () => {
 		setIsMenuOpen((prev) => !prev);
@@ -74,8 +77,9 @@ export default function Header() {
 			<div className="flex items-center gap-6">
 				<div className="flex items-center gap-4">
 					<div className="flex items-center gap-2">
-						<IconSun />
-						<IconMoon />
+						<button onClick={toggleDarkMode}>
+							{isDarkMode ? <IconSun /> : <IconMoon />}
+						</button>
 					</div>
 					<a
 						className="bg-primaryRed-200 text-primaryRed-800 hover:bg-primaryRed-100 hover:border-primaryRed-800 focus:ring-offset-primaryRed-800 dark:hover:border-primaryRed-400 dark:focus:ring-offset-primaryRed-400 cursor-pointer rounded-full border-2 border-transparent px-3 py-2 outline-none transition-all duration-300 ease-in-out focus:ring focus:ring-transparent focus:ring-offset-2 md:px-4 md:py-3"
